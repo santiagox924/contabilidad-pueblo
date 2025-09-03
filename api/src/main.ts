51497d4d935ce7668498f7599878d969ec0e1452
Gox <santiagox924@gmail.com>
2025-09-03 10:21:09 -0500

    backend y frontend conectados

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,20 @@
+// main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors({
+    origin: ['http://localhost:3001', 'http://127.0.0.1:3001'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+  });
+
+  // Si usas prefijo global, recuérdalo en el FRONT
+  // app.setGlobalPrefix('api');
+
+  await app.listen(3000);
 }
 bootstrap();
